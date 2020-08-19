46fa49ef345ccbe36d53e093d3e1d138c30a18e2
yuuki higaki <yuukihigaki@yuukinoMacBook-Pro.local>
2020-08-20 01:27:12 +0900

    都道府県をクリックするとクリックした都道府県がタイトルのダイアログが表示するようにした

--- mapgram/src/components/MapDialog.vue
+++ mapgram/src/components/MapDialog.vue
@@ -4,17 +4,17 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel">{{title}}</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        ...
+          <img src="https://placehold.jp/150x150.png" class="img-rounded img-responsive">
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-primary">Add Photo</button>
       </div>
     </div>
   </div>
@@ -25,12 +25,17 @@
     export default {
         name: 'MapDialog',
         data: () => {
+            return {
+                title:"",
 
+            }
         },
         methods: {
-            openDialog: function(){
+            openDialog: function(reg){
+                this.title = reg;
                 $('#mapDialog').modal('show');
-            }
+            },
+            
         }
     }
 </script>
