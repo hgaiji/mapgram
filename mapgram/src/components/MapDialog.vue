a57a7b2fb24a26df5cfd44c524b16586ffd84df1
yuuki higaki <yuukihigaki@yuukinoMacBook-Pro.local>
2020-08-21 01:53:34 +0900

    ローカルの画像を選択してダイアログにuploadできる機能を追加

--- mapgram/src/components/MapDialog.vue
+++ mapgram/src/components/MapDialog.vue
@@ -11,10 +11,14 @@
       </div>
       <div class="modal-body">
           <img src="https://placehold.jp/150x150.png" class="img-rounded img-responsive">
+          <img v-if="!isNotUploadImage" :src="uploadImageUrl" />
+          <br/>
+          <v-file-input placeholder="Upload your photo" label="Photo input" @change="onImagePicked"></v-file-input>      
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Add Photo</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal" v-on:click="clearImage">Close</button>
+        <button type="button" class="btn btn-primary" v-bind:disabled="!uploadImageUrl" v-on:click="uploadImage">Upload</button>
+        <!-- <label><span type="button" class="btn btn-primary">Add Photo<input type="file" style="display:none"></span></label> -->
       </div>
     </div>
   </div>
@@ -27,15 +31,39 @@
         data: () => {
             return {
                 title:"",
-
+                uploadImageUrl:"",
+                isNotUploadImage: true
             }
+        },
+        computed: {
         },
         methods: {
             openDialog: function(reg){
                 this.title = reg;
                 $('#mapDialog').modal('show');
             },
-            
+            onImagePicked: function(file) {
+                if (file !== undefined && file !== null) {
+                    if (file.name.lastIndexOf('.') <= 0) {
+                    return
+                    }
+                    const fr = new FileReader()
+                    fr.readAsDataURL(file)
+                    fr.addEventListener('load', () => {
+                    this.uploadImageUrl = fr.result
+                    })
+                } else {
+                    console.log("キャンセル");
+                    this.uploadImageUrl = ''
+                    this.isNotUploadImage = true;
+            }
+            },
+            uploadImage: function() {
+              this.isNotUploadImage = false;
+            },
+            clearImage: function() {
+              this.uploadImageUrl = '' 
+            }
         }
     }
 </script>
